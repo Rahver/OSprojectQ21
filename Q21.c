7be558b11280aef636c2b54f2caeb08db237cfc5
Rahver <38882059+Rahver@users.noreply.github.com>
2019-04-14 10:12:14 +0530

    Add files via upload
    Added if else condition if the value of the maximum page fault rate is more than the specified limit.

--- Q21.c
+++ Q21.c
@@ -2,20 +2,32 @@
 int main()
 {
 	float a,b,c,y,z,p,q;
-	printf("enter time (ns) to service page fault (non modified)");
-	scanf("%f",&b);
-	printf("enter time (ns) to service page fault (modified)");
+	//b is the time to service page fault (non modified)
+	//a is the time to service page fault (modified)
+	//c is the memory access time
+	//y is the effective access time
+	//z is the page replace modified percentage
+	//q is ~ of z/100
+	printf("enter time (ns) to service page fault (non modified): ");
+	scanf("%f",&b); 
+	printf("enter time (ns) to service page fault (modified): ");
 	scanf("%f",&a);
 	printf("enter memory access time (ns) : ");
 	scanf("%f",&c);
 	printf("enter effective access time: ");
 	scanf("%f",&y);
-	printf("enter page replace modified % ");
+	printf("enter page replace modified %: ");
 	scanf("%f",&z);
+	
+	
 	z= z/100;
 	q = 1-z;
 	p=(y-c)/((z*a)+(q*b)-(c));
-//	p=(y-c)/((-c)+(z*a)+(q*b));
-//	p=(y-c)/(((z*a)+(1-z)*b)+c);
-	printf("%f",p);
+	if (p< 200)
+	{
+	printf("maximum effective access is : %f",p);
+	}
+	else{
+		printf("maximum effective access is more than 200: %f",p);
+	}
 }
